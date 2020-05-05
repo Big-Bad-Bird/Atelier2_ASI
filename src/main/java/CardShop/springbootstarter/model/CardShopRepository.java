822c2065c2e416a8399fb746b9c3bbb25fba7964
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-05-05 22:12:30 +0200

    Rest repo

--- src/main/java/CardShop/springbootstarter/model/CardShopRepository.java
+++ src/main/java/CardShop/springbootstarter/model/CardShopRepository.java
@@ -1,13 +1,14 @@
 package CardShop.springbootstarter.model;
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 
 
 
-public interface CardShopRepository extends CrudRepository<CardModel, Integer>{
-	public List<CardModel> findById(String id);
+public interface CardShopRepository extends CrudRepository<CardModel, String>{
+	public Optional<CardModel> findById(String id);
+	public CardModel findOne(String name);
 
 }
 
