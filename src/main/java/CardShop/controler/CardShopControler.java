89fe105911a651ab9f5942d0bbc62f193480841e
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-05-05 11:16:17 +0200

    ajout requêtes + services

--- src/main/java/CardShop/controler/CardShopControler.java
+++ src/main/java/CardShop/controler/CardShopControler.java
@@ -1,8 +1,32 @@
 package CardShop.controler;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import CardShop.model.CardModel;
+import CardShop.model.CardShopService;
 
 @RestController
 public class CardShopControler {
+	@Autowired
+	private CardShopService cardShopService;
 	
+	@RequestMapping("/Card/getAllCards")
+	public List<CardModel> getAllCards() {
+		return cardShopService.getallCard();
+	}
+	
+	@RequestMapping("/Card/{id}")
+	public CardModel getCardById(@PathVariable String id) {
+		return cardShopService.getCardById(id);
+	}
+	
+	@RequestMapping("/Card/addCard/{card}")
+	public void addCard(@PathVariable CardModel card) {
+		cardShopService.addCard(card);
+	}
 }
