f24b7c9548e065391ce1f806b7dd5b066e24242b
LucieCharrier <lucie.charrier@cpe.fr>
2020-05-05 16:08:28 +0200

    Ajout de toutes les sources et target user

---
+++ user_src_target/src/main/java/user/AppUser.java
@@ -0,0 +1,13 @@
+package user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUser {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppUser.class,args);
+	}
+
+}
